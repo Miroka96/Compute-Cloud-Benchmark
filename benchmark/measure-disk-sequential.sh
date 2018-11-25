c7769f899bd616f31fbc8a23e929b1cab9fff3c2
Mirko Krause <mirko.krause@student.hpi.de>
2018-11-25 21:45:10 +0100

    fixed sequential benchmark

--- benchmark/measure-disk-sequential.sh
+++ benchmark/measure-disk-sequential.sh
@@ -3,16 +3,21 @@
 # Testing write
 # We write 8 times 250 MB of null characters and write them to seq_write.
 # After that we flush the cache and stop measuring
-WRITE=$(dd if=/dev/zero of=seq_write bs=250M count=8 conv=fdatasync 2>&1 | tail -n 1 | 
-cut -d " " -f 10-11 | awk '{if($2 ~ /GB/){print $1 * 1000000000}else{print $1 * 1000000}}')
+WRITE=$(dd if=/dev/zero of=seq_write bs=250M count=4 conv=fdatasync 2>&1 | tail -n 1 | 
+cut -d " " -f 10-11 | sed s/,/./g | awk '{if($2 ~ /GB/){printf "%d000000\n", $1 * 1000}else{printf "%d000000\n", $1}}')
+
+sync
+
 # Testing read
 # We read from our 2GB file in blocks of 250 so we are not bottlencked by memory
 # We flush at the end so the system can optimize during write, but the result is
 # only measured after everything is written to disk
-READ=$(dd if=seq_write of=/dev/null bs=250M count=8 conv=fdatasync | 2>&1 | tail -n 1 | 
-cut -d " " -f 10-11 | awk '{if($2 ~ /GB/){print $1 * 1000000000}else{print $1 * 1000000}}')
+READ=$(dd if=seq_write of=/dev/null bs=250M count=4 2>&1 | tail -n 1 | 
+cut -d " " -f 10-11 | sed s/,/./g | awk '{if($2 ~ /GB/){printf "%d000000\n", $1 * 1000}else{printf "%d000000\n", $1}}')
 # We measure the performance as the sum of written and read MB per second
 
 rm seq_write
 
+echo $READ
+echo $WRITE
 echo "$WRITE + $READ" | bc
