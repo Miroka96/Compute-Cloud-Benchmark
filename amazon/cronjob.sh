ffa4ae7d58fd78ee9ef829620115a227a4bcf9c0
Mirko Krause <mirko.krause@student.hpi.de>
2018-11-26 21:41:36 +0100

    improved amazon cronjob time measuring and fastened seq disk benchmark

--- amazon/cronjob.sh
+++ amazon/cronjob.sh
@@ -1,38 +1,44 @@
-#!/bin/bash -e
+#!/bin/bash
 # automate with: 0 * * * *	cd /root/Compute-Cloud-Benchmark/amazon/ && ./cronjob.sh i-0eb40592c3e1df399 2>&1 > /root/cronerror.log
 
 source ./env
-
-env
 
 echo started
 
 CLOUD=ec2
 
-./start-instance.sh $1
+echo "Trying to start instance $1"
+time ./start-instance.sh $1
 
 echo instance started
-sleep 10
+
+UP=1
+while [ $UP -gt 0 ]
+do echo "is it up?"
+	time ./open-ssh.sh $1 echo "instance now up" > /dev/null
+	UP=$?
+	sleep 1
+done
 
 echo instance hopefully up
-CPU=$(./open-ssh.sh $1 ./linpack.sh)
+CPU=$(time ./open-ssh.sh $1 ./linpack.sh)
 echo "$(date +%s),$CPU" >> $CLOUD-cpu.csv
 echo "benchmarked CPU"
 
-RAM=$(./open-ssh.sh $1 ./memsweep.sh)
+RAM=$(time ./open-ssh.sh $1 ./memsweep.sh)
 echo "$(date +%s),$RAM" >> $CLOUD-mem.csv
 echo "benchmarked RAM"
 
-IOS=$(./open-ssh.sh $1 ./measure-disk-sequential.sh)
+IOS=$(time ./open-ssh.sh $1 ./measure-disk-sequential.sh)
 echo "$(date +%s),$IOS" >> $CLOUD-disk-sequential.csv
 echo "benchmarked IO seq"
 
-IOR=$(./open-ssh.sh $1 ./measure-disk-random.sh)
+IOR=$(time ./open-ssh.sh $1 ./measure-disk-random.sh)
 echo "$(date +%s),$IOR" >> $CLOUD-disk-random.csv
 echo "benchmarked IO rand"
 
 echo "benchmarking done"
 
-./stop-instance.sh $1
+time ./stop-instance.sh $1
 
 echo "instance stopped"
--- benchmark/measure-disk-sequential.sh
+++ benchmark/measure-disk-sequential.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
+BS=100M
+COUNT=3
+
 # Testing write
 # We write 8 times 250 MB of null characters and write them to seq_write.
 # After that we flush the cache and stop measuring
-WRITE=$(dd if=/dev/zero of=seq_write bs=200M count=3 conv=fdatasync 2>&1 | tail -n 1 | 
+WRITE=$(dd if=/dev/zero of=seq_write bs=$BS count=$COUNT conv=fdatasync 2>&1 | tail -n 1 | 
 cut -d " " -f 8-9 | sed s/,/./g | awk '{if($2 ~ /GB/){printf "%d000000\n", $1 * 1000}else{printf "%d000000\n", $1}}')
 
 sync
@@ -12,7 +15,7 @@ sync
 # We read from our 2GB file in blocks of 250 so we are not bottlencked by memory
 # We flush at the end so the system can optimize during write, but the result is
 # only measured after everything is written to disk
-READ=$(dd if=seq_write of=/dev/null bs=200M count=3 2>&1 | tail -n 1 | 
+READ=$(dd if=seq_write of=/dev/null bs=$BS count=$COUNT 2>&1 | tail -n 1 | 
 cut -d " " -f 8-9 | sed s/,/./g | awk '{if($2 ~ /GB/){printf "%d000000\n", $1 * 1000}else{printf "%d000000\n", $1}}')
 # We measure the performance as the sum of written and read MB per second
 
